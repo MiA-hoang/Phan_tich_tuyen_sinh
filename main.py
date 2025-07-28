87ce28f160a8f41750ca58b958943f00405e7f97
Gin <hoangngocha11122k6@gmail.com>
2025-07-28 09:46:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,14 @@ from routes.admission_data import admission_data_bp
 app = Flask(__name__)
 
 app.register_blueprint(app_routes)
+app.register_blueprint(university_bp, url_prefix='/api')
+app.register_blueprint(major_bp, url_prefix='/api')
+app.register_blueprint(exam_group_bp, url_prefix='/api')
+app.register_blueprint(admission_data_bp, url_prefix='/api')
 
-
-# Đăng ký blueprint từ routes với tiền tố /api/ để tránh xung đột URL 
-app.register_blueprint(university_bp, url_prefix='/api/universities')
-app.register_blueprint(major_bp, url_prefix='/api/majors')
-app.register_blueprint(exam_group_bp, url_prefix='/api/exam_groups')
-app.register_blueprint(admission_data_bp, url_prefix='/api/admission_data')
-
-# Trang chủ
 @app.route("/")
 def home():
     return "Welcome to Tuyển Sinh API!"
+
 if __name__ == "__main__":
     app.run(debug=True)
